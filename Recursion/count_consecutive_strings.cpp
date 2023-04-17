488456f5b65c7c6c901a81cba38ed26a1f78b1dc
soumyajit0 <soumyajitdeysarkar@gmail.com>
2023-04-17 21:28:54 +0530

    Recursion Complete

---
+++ Recursion/count_consecutive_strings.cpp
@@ -0,0 +1,66 @@
+/*
+    @author:     soumyajit_0
+    @codechef:   https://www.codechef.com/users/soumyajit_0
+    @codeforces: https://codeforces.com/profile/soumyajit_0
+    @leetcode:   https://leetcode.com/soumyajit_0/
+    @linkedin:   https://www.linkedin.com/in/soumyajitds/
+    @github:     https://github.com/soumyajit0
+*/
+
+#include<bits/stdc++.h>
+
+#define int      long long int
+#define print(a) for (auto x : a) cout << x << " "; cout << endl
+#define endl     '\n'
+#define bug(...) __f (#__VA_ARGS__, __VA_ARGS__)
+
+using namespace std;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int MOD=1e9+7;
+
+int count(int n){
+    if (n<=2)
+        return n+1;
+    return count(n-1)+count(n-2);
+}
+
+void solve(){
+
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    cout << "No. of Non-Consecutive Strings : " << count(n) << endl;
+
+}
+
+int32_t main (){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+    clock_t start_time=clock();
+
+    int t=1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    clock_t end_time=clock();
+
+    cerr << "RUN TIME : " << end_time - start_time << "ms ";
+
+    return 0;
+}
---
+++ Recursion/tower_of_hanoi.cpp
@@ -0,0 +1,70 @@
+/*
+    @author:     soumyajit_0
+    @codechef:   https://www.codechef.com/users/soumyajit_0
+    @codeforces: https://codeforces.com/profile/soumyajit_0
+    @leetcode:   https://leetcode.com/soumyajit_0/
+    @linkedin:   https://www.linkedin.com/in/soumyajitds/
+    @github:     https://github.com/soumyajit0
+*/
+
+#include<bits/stdc++.h>
+
+#define int      long long int
+#define print(a) for (auto x : a) cout << x << " "; cout << endl
+#define endl     '\n'
+#define bug(...) __f (#__VA_ARGS__, __VA_ARGS__)
+
+using namespace std;
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int MOD=1e9+7;
+
+void towerOfHanoi(int n,char from,char to,char aux){
+    if (n==1){
+        cout << "Move Disk " << n << " from " << from << " to " << to << endl;
+        return;
+    }
+    towerOfHanoi(n-1,from,aux,to);
+    cout << "Move Disk " << n << " from " << from << " to " << to << endl;
+    towerOfHanoi(n-1,aux,to,from);
+}
+
+void solve(){
+
+    int n;
+    cout << "Enter the no. of Disks : ";
+    cin >> n;
+    towerOfHanoi(n,'A','C','B');
+
+}
+
+int32_t main (){
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+    clock_t start_time=clock();
+
+    int t=1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    clock_t end_time=clock();
+
+    cerr << "RUN TIME : " << end_time - start_time << "ms ";
+
+    return 0;
+}
